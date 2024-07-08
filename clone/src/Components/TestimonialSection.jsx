82d2190c7284f883b984be5e712c0acb7da929f2
Naman Jain <nj626433@gmail.com>
2024-07-08 20:20:40 +0530

    new commit in testimonials

--- clone/src/Components/TestimonialSection.jsx
+++ clone/src/Components/TestimonialSection.jsx
@@ -6,15 +6,15 @@ import add from '../assets/Add.png'
 function TestimonialSection() {
   return (
     <>
-    <div className='border mt-4 ml-0 py-8  md:p-10 w-[300px] md:w-full bg-sky-100 '>
-      <div className='w-[280px] md:w-[900px] m-auto bg-white p-2 md:p-4'>
-        <div className='flex gap-2'>
-          <img src={add} alt="" />
-          <h1 className=' ml-2 mt-1'>Add your own</h1>
+      <div className='border mt-4 ml-0 py-8  md:p-10 w-[300px] md:w-full bg-sky-100 '>
+        <div className='w-[280px] md:w-[900px] m-auto bg-white p-2 md:p-4'>
+          <div className='flex gap-2'>
+            <img src={add} alt="" />
+            <h1 className=' ml-2 mt-1'>Add your own</h1>
+          </div>
+          <p className='mt-2'>Are you a teacher or expert?  Do you sell or rent out equipment, venue or event tickets?  Or, you know someone who should be on hobbycue?  Go ahead and Add your Own page</p>
+          <button className='py-1 px-5 border mt-2 ' >Add New</button>
         </div>
-        <p className='mt-2'>Are you a teacher or expert?  Do you sell or rent out equipment, venue or event tickets?  Or, you know someone who should be on hobbycue?  Go ahead and Add your Own page</p>
-        <button className='py-1 px-5 border mt-2 ' >Add New</button>
-      </div>
       </div>
 
 
@@ -27,7 +27,7 @@ function TestimonialSection() {
           </div>
 
           <p className="mt-2">"In a fast growing and ever changing city like Bangalore, it sometimes becomes very difficult to find or connect with like minded people. Websites like hobbycue.com is a great service which helps me get in touch with, communicate, connect, and exchange ideas with other dancers. It also provides the extra benefit of finding products and services that I can avail, which I can be assured is going to be of great quality as it comes recommended by people of the hobbycue community. To have discussions, to get visibility, and to be able to safely explore various hobbies and activities in my city, all under one roof, is an excellent idea and I highly recommend it."</p>
-          <div className='flex flex-col-reverse md:felx-row ' >
+          <div className='flex md:flex-row flex-col-reverse' >
             <img src={audio} alt="" className='h-[60px] w-[350px]  md:w-[550px] md:object-cover mt-5 ' />
             <div className='flex  md:flex-row '>
               <img src={Ellipse} alt="" className='h=[30px] w-[60px] md:ml-20 mt-5 ' />
